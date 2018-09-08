7913e19f1eb96a2ae5d2cd7adab108233af27b3a
Galilée 'Bill' Enguehard <galilee.enguehard@gmail.com>
2018-09-08 18:59:11 +0200

    Changed layering triggers
    Reverted to momentary triggers for layers
    Discarded layer switching and key combos on regular letter and symbol
    keys

--- keyboards/ergodox_infinity/keymaps/san/keymap.c
+++ keyboards/ergodox_infinity/keymaps/san/keymap.c
@@ -43,20 +43,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,        KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   TG(SYMB),
         KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_GRV,
         KC_LCTRL,      KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,       CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+        KC_LSFT,       KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
         KC_NO,         KC_NO,        KC_NO,  KC_LGUI,KC_LALT,
                                               ALT_T(KC_APP),  KC_PGUP,
                                                               KC_PGDOWN,
-                                               KC_SPC,KC_ESC,TT(MDIA),
+                                               KC_SPC,KC_ESC,MO(MDIA),
         // right hand
              TG(MDIA),    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              KC_NO,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
+                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                                   KC_RALT,KC_RGUI,KC_LBRC,KC_RBRC,KC_RCTL,
              KC_HOME,             KC_DEL,
              KC_END,
-             TT(SYMB), KC_BSPC, KC_ENT
+             MO(SYMB), KC_BSPC, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
